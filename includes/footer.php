95439195f1fc76f8ce4bda337d2d88277beb95fe
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2021-02-07 01:54:41 +0530

    update

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,7 @@
       <div class="pull-right hidden-xs">
         <b>Voting System</b>
       </div>
-      <strong> &copy; 2018 Brought To You By<a href="https://code-projects.org/"> Code-Projects</a></strong>
+      <strong> &copy; 2018 Brought To You By VotingSystem</a></strong>
     </div>
     <!-- /.container -->
 </footer>
